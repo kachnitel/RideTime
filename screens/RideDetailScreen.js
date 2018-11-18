04c56565ce7412285eae4c735a7d31a4ed2a45bd
Ondrej Vana <vana.ondrej@gmail.com>
2018-11-18 00:59:39 -0800

    dynamic ride title in ride detail

--- screens/RideDetailScreen.js
+++ screens/RideDetailScreen.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { Text } from 'react-native';
 
 export default class RideDetailScreen extends React.Component {
-  static navigationOptions = {
-    headerTitle: 'That ride'
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: navigation.getParam('name', 'NO NAME RIDE'),
+    };
   };
 
   render() {
     return (
-      <Text>Name: {this.props.navigation.getParam('name', 'NO NAME')}</Text>
+      <Text>Name: {this.props.navigation.getParam('name', 'NO NAME RIDE')}</Text>
     );
   }
 }
