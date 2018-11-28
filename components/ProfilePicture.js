44d365fb2b9aa3145ed4aa6600a05d9096ce53d8
Ondrej Vana <vana.ondrej@gmail.com>
2018-11-28 00:26:25 -0800

    work on RiderItem use S3 url to retrieve image

---
+++ components/ProfilePicture.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Image, View, StyleSheet } from 'react-native';
+
+export default class ProfilePicture extends React.Component {
+  loadImageAsync(riderId) {
+
+  }
+
+  render() {
+    riderId =  this.props.rider.id;
+    imgPath = 'https://s3.ca-central-1.amazonaws.com/ride-time/profile-images/' + riderId + '.png';
+
+    return (
+      <Image 
+        source={{uri: imgPath}}
+        style={styles.image}
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  image: {
+    width: 80,
+    height: 80,
+    borderRadius: 50,
+    backgroundColor: 'rgba(12, 94, 20, 0.5);',
+    margin: 10
+  }
+});
--- components/list_items/RiderItem.js
+++ components/list_items/RiderItem.js
@@ -2,18 +2,19 @@ import React from 'react';
 import { Text, View, TouchableHighlight, StyleSheet } from 'react-native';
 import { withNavigation } from 'react-navigation';
 import { ListItem } from './ListItem';
+import ProfilePicture from '../ProfilePicture';
 
 class RiderItem extends ListItem {
   render() {
     return (
+      // Rider should contain his details fetched in list
+      // To render eg. little exp. icon in corner of image
       <TouchableHighlight onPress={() => this.props.navigation.navigate('PublicProfile', this.props.rider)}>
-      {/* <TouchableHighlight> */}
         <View style={styles.listItem}>
-          {<Text style={styles.name}>
+          <ProfilePicture rider={this.props.rider} />
+          <Text style={styles.name}>
             {this.props.rider.name}
-          </Text>}
-          {/* Rider should contain his details fetched in list */}
-          {/* <RiderItemDetail ride={this.props.rider} /> */}
+          </Text>
         </View>
       </TouchableHighlight>
     );
@@ -24,7 +25,9 @@ export default withNavigation(RiderItem);
 
 const styles = StyleSheet.create({
   listItem: {
-    width: 100
+    width: 100,
+    height: 120,
+    justifyContent: 'center'
   },
   name: {
     color: '#fff',
