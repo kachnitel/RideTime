6946338a1762aee3b2ab0759b89f01a9810c0216
Ondrej Vana <vana.ondrej@gmail.com>
2018-11-28 19:00:27 -0800

    cleanup App.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { AppLoading, Asset, Font, Icon } from 'expo';
+import { AppLoading, Font, Icon } from 'expo';
 import AppNavigator from './navigation/AppNavigator';
 
 export default class App extends React.Component {
@@ -29,28 +29,22 @@ export default class App extends React.Component {
 
   _loadResourcesAsync = async () => {
     return Promise.all([
-      // Asset.loadAsync([
-        // require('./assets/images/robot-dev.png'),
-        // require('./assets/images/robot-prod.png'),
-      // ]),
       Font.loadAsync({
         // This is the font that we are using for our tab bar
         ...Icon.Ionicons.font,
-        // We include SpaceMono because we use it in HomeScreen.js. Feel free
-        // to remove this if you are not using it in your app
-        'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
         'trail-difficulty-icons': require('./assets/fonts/trail-difficulty-icons.ttf'),
         'ride-time-icons': require('./assets/fonts/ride-time-icons.ttf'),
+        'Roboto': require('./assets/fonts/roboto/Roboto-Regular.ttf')
       }),
     ]);
   };
-
+  
   _handleLoadingError = error => {
     // In this case, you might want to report the error to your error
     // reporting service, for example Sentry
     console.warn(error);
   };
-
+  
   _handleFinishLoading = () => {
     this.setState({ isLoadingComplete: true });
   };
