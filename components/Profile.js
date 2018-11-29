fea6c27e2a2e503267e116b4fb4906eb0c4d0125
Ondrej Vana <vana.ondrej@gmail.com>
2018-11-29 00:49:14 -0800

    cleanup: Profile imports

--- components/Profile.js
+++ components/Profile.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
-import CoverPicture from './CoverPicture';
-import Dimensions from '../constants/Layout'
-import ProfilePicture from './ProfilePicture';
-import Colors from '../constants/Colors';
 import ProfileHeader from './ProfileHeader';
 
 export default class Profile extends React.Component {
