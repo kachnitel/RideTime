582c86dc84b661198506574967f1e8b8ad540584
Ondrej Vana <vana.ondrej@gmail.com>
2018-11-29 00:48:27 -0800

    refactor: Simplify RiderProvider

--- providers/RidersProvider.js
+++ providers/RidersProvider.js
@@ -5,22 +5,10 @@ import users from '../userList.json';
 
 export default class RidersProvider {
     static getUsers() { //only static until we're actually pulling data
-        return(RidersProvider.convertFromList(users));
+        return(users);
     }
 
     static getUser(id) {
       return(RidersProvider.getUsers()[id]);
-    }
-
-    static convertFromList(users) {
-        var usersById = {};
-        users.forEach(user => {
-          usersById[user.id] = {
-            id: user.id,
-            name: user.name
-          }
-        });
-        
-        return usersById;
     }
 }
