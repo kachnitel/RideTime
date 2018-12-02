02fc13e1d7132c4046b50ef27a88371d4c0a23cb
Ondrej Vana <vana.ondrej@gmail.com>
2018-12-02 01:43:18 -0800

    experiment w/ RiderLevel style

--- components/profile-summary/RiderLevel.js
+++ components/profile-summary/RiderLevel.js
@@ -5,6 +5,12 @@ import sharedStyles from './Style';
 import Title from './Title';
 import Colors from '../../constants/Colors';
 
+/**
+ * TODO: Need to discuss design options
+ * - iconBg size 28, icon on top
+ * - iconBg size 24 ONLY
+ * - iconBg size 28, icon on top in "real" color
+ */
 export default class RiderLevel extends Component {
   render() {
     return(
@@ -12,8 +18,8 @@ export default class RiderLevel extends Component {
         <View style={styles.summaryItem}>
           <Title style={styles.title}>Level</Title>
           <View style={styles.content}>
-            <DifficultyIcon d={this.props.level} size={28} style={{...styles.icon, ...styles.iconBg}} />
-            <DifficultyIcon d={this.props.level} size={24} style={styles.icon} />
+            <DifficultyIcon d={this.props.level} size={26} style={{...styles.icon, ...styles.iconBg}} />
+            <DifficultyIcon d={this.props.level} size={21} style={styles.icon} />
           </View>
         </View>
       : null
@@ -24,7 +30,7 @@ export default class RiderLevel extends Component {
 const styles = StyleSheet.create({
   ...sharedStyles,
   icon: {
-    // color: Colors.darkBackground,
+    color: Colors.darkBackground,
     position: 'absolute',
     alignSelf: "center",
     justifyContent: 'center'
