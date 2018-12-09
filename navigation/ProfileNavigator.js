adedaa663fc291d795ccb0c1ca927381c9c00ae5
Ondrej Vana <vana.ondrej@gmail.com>
2018-12-09 12:22:22 -0800

    fix: Also return from upcoming ride to My Profile

--- navigation/ProfileNavigator.js
+++ navigation/ProfileNavigator.js
@@ -1,12 +1,14 @@
 import { createStackNavigator } from 'react-navigation'; // Version can be specified in package.json
 import OwnProfileScreen from '../screens/OwnProfileScreen';
 import PublicProfileScreen from '../screens/PublicProfileScreen';
+import RideDetailScreen from '../screens/RideDetailScreen';
 
 const ProfileStack = createStackNavigator(
   {
     Profile: OwnProfileScreen,
     // EditProfile: EditProfileScreen,
-    PublicProfile: {screen: PublicProfileScreen}
+    PublicProfile: {screen: PublicProfileScreen},
+    RideDetail: {screen: RideDetailScreen}
   },
   {
     initialRouteName: 'Profile',
