ebe167221900f2e6bc4c4590a6373947e9001b55
Ondrej Vana <vana.ondrej@gmail.com>
2018-12-09 12:20:09 -0800

    fix: Return to "My Profile" after going to friend

--- navigation/ProfileNavigator.js
+++ navigation/ProfileNavigator.js
@@ -1,10 +1,12 @@
 import { createStackNavigator } from 'react-navigation'; // Version can be specified in package.json
 import OwnProfileScreen from '../screens/OwnProfileScreen';
+import PublicProfileScreen from '../screens/PublicProfileScreen';
 
 const ProfileStack = createStackNavigator(
   {
     Profile: OwnProfileScreen,
-    // EditProfile: EditProfileScreen
+    // EditProfile: EditProfileScreen,
+    PublicProfile: {screen: PublicProfileScreen}
   },
   {
     initialRouteName: 'Profile',
--- screens/OwnProfileScreen.js
+++ screens/OwnProfileScreen.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Profile from '../components/profile/Profile';
 import { Text, View } from 'react-native';
+import RidersProvider from '../providers/RidersProvider';
 
 export default class OwnProfileScreen extends React.Component {
   static navigationOptions = ({ navigation }) => {
     return {
-      // w/ little name under in drawer eventually 
+      // w/ little name under in drawer eventually
       title: 'My Profile',
       headerRight: (
         <Text>Edit</Text>
@@ -14,10 +15,7 @@ export default class OwnProfileScreen extends React.Component {
   };
 
   render() {
-    user = {
-      id: 4,
-      name: "Hardcoded user"
-    }
+    user = RidersProvider.getUser(1);
     return (
       <Profile user={user} />
     );
