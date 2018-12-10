0a3282a5cdd417736b613f51d8e7d65b40bb9e32
Ondrej Vana <vana.ondrej@gmail.com>
2018-12-09 23:36:59 -0800

    style: Reduce header on PublicProfileScreen.js

--- screens/PublicProfileScreen.js
+++ screens/PublicProfileScreen.js
@@ -2,11 +2,15 @@ import React from 'react';
 import Profile from '../components/profile/Profile';
 
 // TODO OwnProfileScreen, title just Profile or My Profile
-// w/ little name under in sidebar eventually 
+// w/ little name under in sidebar eventually
 export default class PublicProfileScreen extends React.Component {
   static navigationOptions = ({ navigation }) => {
     return {
-      title: 'Profile: ' + navigation.getParam('name', 'NAMELESS RIDER')
+      // title: 'Profile: ' + navigation.getParam('name', 'NAMELESS RIDER'),
+      headerStyle: {backgroundColor: 'rgba(255,255,255,0.66);', width: 55, height: 50, borderBottomRightRadius: 5},
+      // header: null,
+      headerTransparent: true,
+      // headerTintColor: '#FFF'
     };
   };
 
