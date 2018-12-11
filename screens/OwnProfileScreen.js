39f2c47ddcb5a9aeb10a2a32d1ff6381d3b584ff
Ondrej Vana <vana.ondrej@gmail.com>
2018-12-11 02:15:58 -0800

    fix: Drawer menu on My profile

--- screens/OwnProfileScreen.js
+++ screens/OwnProfileScreen.js
@@ -4,6 +4,8 @@ import Profile from '../components/profile/Profile';
 import RidersProvider from '../providers/RidersProvider';
 import Layout from '../constants/Layout';
 import Colors from '../constants/Colors';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { TouchableHighlight } from 'react-native';
 
 export default class OwnProfileScreen extends React.Component {
   static editProfile() {
@@ -22,6 +24,14 @@ export default class OwnProfileScreen extends React.Component {
           color={Colors.tintColor}
         />
         </View>
+      ),
+      headerLeft: (
+        <TouchableHighlight
+          onPress={() => navigation.toggleDrawer()}>
+          <View style={styles.headerMenuIconContainer}>
+            <Icon style={styles.headerMenuIcon} name='menu' />
+          </View>
+        </TouchableHighlight>
       )
     };
   };
@@ -37,5 +47,13 @@ export default class OwnProfileScreen extends React.Component {
 const styles = StyleSheet.create({
   button: {
     paddingRight: Layout.window.wp(2),
+  },
+  headerMenuIconContainer: {
+    justifyContent: 'center'
+  },
+  headerMenuIcon: {
+    fontSize: 24,
+    paddingLeft: 15,
+    color: '#0C5E14'
   }
 });
