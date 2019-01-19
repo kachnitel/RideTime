f0a1979e73904a584d9a5db3df6afcbc3d13e42d
Ondrej Vana <vana.ondrej@gmail.com>
2019-01-19 14:16:26 -0800

    style: LocationItem closer to RideItem

--- components/list_items/LocationItem.js
+++ components/list_items/LocationItem.js
@@ -7,7 +7,7 @@ import PropTypes from 'prop-types'
 export default class LocationItem extends React.Component {
   render () {
     return (
-      <View style={{ ...styles.listItem, ...this.props.style }}>
+      <View style={{ ...styles.container, ...this.props.style }}>
         <Text style={{ ...styles.name, ...this.props.style }} numberOfLines={1} >
           {this.props.location.name}
         </Text>
@@ -25,10 +25,9 @@ LocationItem.propTypes = {
 const styles = StyleSheet.create({
   name: {
     fontSize: Layout.window.hp(2.75),
-    fontWeight: 'bold',
     flex: 1
   },
-  listItem: {
+  container: {
     height: Layout.window.hp(15),
     paddingVertical: Layout.window.hp(1.5),
     paddingHorizontal: Layout.window.wp(4)
