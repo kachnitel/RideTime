1e1d848ab17766e278b93f7e962d1e8eb16c3833
Ondrej Vana <vana.ondrej@gmail.com>
2019-01-19 14:48:11 -0800

    style: CreateRideScreen Ride name input

--- screens/CreateRideScreen.js
+++ screens/CreateRideScreen.js
@@ -1,7 +1,18 @@
 import React from 'react'
 import { StyleSheet, View, TextInput } from 'react-native'
 import SelectDifficulty from '../components/new_ride/SelectDifficulty'
+import Layout from '../constants/Layout'
 
+/**
+ * Setup ride here - difficulty, trails, friends, ...
+ *
+ * TODO: Done button
+ * TODO: Add a little "edit" icon at TextInput end
+ *
+ * @export
+ * @class CreateRideScreen
+ * @extends {React.Component}
+ */
 export default class CreateRideScreen extends React.Component {
   static navigationOptions = ({ navigation }) => {
     return {
@@ -12,11 +23,12 @@ export default class CreateRideScreen extends React.Component {
   render () {
     return (
       <View style={styles.container}>
-        {/* Setup ride here - difficulty, trails, friends, ... */}
-        <TextInput>{this.props.navigation.getParam('name', 'NO NAME PLACE')} ride</TextInput>
+        <TextInput
+          style={styles.rideNameInput}
+          placeholder='Ride name'
+          value={this.props.navigation.getParam('name') + ' ride'}
+        />
         <SelectDifficulty onSelect={this.setDifficulty} />
-        {/* <Text style={styles.content}>Hello!</Text> */}
-        {/* Done button */}
       </View>
     )
   }
@@ -26,5 +38,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1
     // paddingTop: Constants.statusBarHeight // TODO enable once header is disabled
+  },
+  rideNameInput: {
+    alignSelf: 'stretch',
+    fontWeight: 'bold',
+    fontSize: Layout.window.hp(3.5),
+    padding: Layout.window.hp(1.5),
+    width: 'auto'
   }
 })
