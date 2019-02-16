e243fd8e91c908ce1f75e62f0a2dfdcd68e810f8
Ondrej Vana <vana.ondrej@gmail.com>
2019-02-15 20:22:06 -0800

    refactor: Move auth logic to Authentication class

--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -1,137 +1,15 @@
-/* global fetch */
-import { AuthSession, SecureStore } from 'expo'
 import React from 'react'
 import {
-  Alert,
   Button,
   StyleSheet,
   Text,
   View
 } from 'react-native'
-import {
-  auth0ClientId,
-  auth0Domain,
-  auth0Audience,
-  auth0RedirectUri
-} from '../secrets'
-import RidersProvider from '../providers/RidersProvider'
-import randomatic from 'randomatic'
-// import jwtDecoder from 'jwt-decode'
+import Authentication from '../src/Authentication'
 
-/*
-  TODO:
-  If you use Facebook through Auth0, be sure to follow this guide: https://auth0.com/docs/connections/social/facebook
-*/
 export default class SignInScreen extends React.Component {
   state = {
     username: undefined
-  }
-
-  loginWithAuth0 = async () => {
-    const redirectUrl = AuthSession.getRedirectUrl()
-    const oAuthState = randomatic('Aa0', 7)
-    const codeVerifier = randomatic('Aa0', 50)
-
-    // console.log(`Redirect URL (add this to Auth0): ${redirectUrl}`)
-    const result = await AuthSession.startAsync({
-      authUrl: `${auth0Domain}/authorize` + this.toQueryString({
-        client_id: auth0ClientId,
-        response_type: 'code',
-        scope: 'openid profile email offline_access',
-        redirect_uri: redirectUrl,
-        code_challenge_method: 'S256',
-        code_verifier: codeVerifier,
-        state: oAuthState,
-        audience: auth0Audience
-      })
-    })
-
-    // console.log('Result /authorize:', result)
-    if (result.type === 'success') {
-      if (oAuthState !== result.params.state) {
-        throw new Error('OAuth state mismatch')
-      }
-
-      let token = await this.getOAuthToken(codeVerifier, result.params.code)
-      this.handleParams(token)
-      let userInfo = await this.getUserInfo(token.access_token)
-
-      // console.log(token, userInfo)
-      // TODO:
-      // fetch user UPSERT to DB;
-      let provider = new RidersProvider(token.access_token)
-      provider.signIn(userInfo)
-        .then((result) => {
-          console.log('API signin result', result)
-          // this.setState({ user: result })
-        })
-
-      // fetch updated user (result above) from DB and store in RN AsyncStorage & "global" state
-      // await AsyncStorage.setItem('signedInUserId', '1') // Use SecureStorage?
-      // this.props.navigation.navigate('App')
-    } else {
-      console.log('Auth0 login Result type: ', result.type)
-    }
-  }
-
-  getOAuthToken = async (codeVerifier, code) => {
-    const rawResponse = await fetch(`${auth0Domain}/oauth/token`, {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        grant_type: 'authorization_code',
-        client_id: auth0ClientId,
-        code_verifier: codeVerifier,
-        code: code,
-        redirect_uri: auth0RedirectUri
-      })
-    })
-    const content = await rawResponse.json()
-
-    // console.log('Result /oauth/token: ', content)
-    return content
-  }
-
-  handleParams = async (responseObj) => {
-    if (responseObj.error) {
-      Alert.alert('Error', responseObj.error_description ||
-      'something went wrong while logging in')
-      return
-    }
-
-    const {
-      // id_token: idToken,
-      access_token: accessToken,
-      refresh_token: refreshToken
-    } = responseObj
-
-    SecureStore.setItemAsync('access_token', accessToken)
-    SecureStore.setItemAsync('refresh_token', refreshToken)
-  }
-
-  getUserInfo = async (apiToken) => {
-    let response = await fetch(`${auth0Domain}/userinfo`, {
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + apiToken
-      }
-    })
-    let user = await response.json()
-
-    return user
-  }
-
-  /**
-   * Converts an object to a query string.
-   */
-  toQueryString (params) {
-    return '?' + Object.entries(params)
-      .map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
-      .join('&')
   }
 
   render () {
@@ -141,7 +19,7 @@ export default class SignInScreen extends React.Component {
           ? <Text style={styles.title}>Hi {this.state.username}!</Text>
           : <View>
             <Text style={styles.title}>Example: Auth0 login</Text>
-            <Button title='Login with Auth0' onPress={this.loginWithAuth0} />
+            <Button title='Login with Auth0' onPress={new Authentication().loginWithAuth0} />
           </View>
         }
       </View>
