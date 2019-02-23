2fefbccd39ce274e234117365eaf1597a0bb74f1
Ondrej Vana <vana.ondrej@gmail.com>
2019-02-23 01:34:47 -0800

    fix: Correct Button class name

--- components/Button.js
+++ components/Button.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
-import { Button } from 'react-native'
+import { Button as RNButton } from 'react-native'
 import Colors from '../constants/Colors'
 
-export default class Header extends Component {
+export default class Button extends Component {
   render () {
     return (
-      <Button {...this.props} color={this.props.color || Colors.tintColor} />
+      <RNButton {...this.props} color={this.props.color || Colors.tintColor} />
     )
   }
 }
