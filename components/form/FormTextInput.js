ced5f0822297984d1c1e8d85d40015352caf1b7a
Ondrej Vana <vana.ondrej@gmail.com>
2019-03-09 12:09:12 -0800

    style: Style focused FormTextInput

--- components/form/FormTextInput.js
+++ components/form/FormTextInput.js
@@ -5,15 +5,36 @@ import Colors from '../../constants/Colors'
 import PropTypes from 'prop-types'
 
 export default class FormTextInput extends React.Component {
+  state = {
+    focused: false
+  }
+
+  onFocus = () => {
+    this.setState({ focused: true })
+  }
+
+  onBlur = () => {
+    this.setState({ focused: false })
+  }
+
   render () {
     let inputStyle = this.props.required && !this.props.value
       ? { ...this.props.style, ...styles.input, borderBottomColor: 'red' }
       : { ...this.props.style, ...styles.input }
+
+    if (this.state.focused) {
+      inputStyle = {
+        ...inputStyle,
+        ...styles.inputFocused
+      }
+    }
     return (
       <TextInput
         {...this.props}
         style={{ ...inputStyle, ...this.props.style }}
         placeholderTextColor='#666'
+        onFocus={this.onFocus}
+        onBlur={this.onBlur}
       />
     )
   }
@@ -29,11 +50,16 @@ FormTextInput.propTypes = {
 const styles = StyleSheet.create({
   input: {
     fontSize: Layout.window.hp(2.5),
-    borderBottomColor: Colors.tintColor,
+    borderColor: Colors.tintColor,
     borderBottomWidth: 1,
     color: '#fff',
     padding: Layout.window.hp(1),
     width: Layout.window.wp(65),
     backgroundColor: 'rgba(255,255,255,0.03);'
+  },
+  inputFocused: {
+    borderRadius: Layout.window.hp(0.5),
+    borderWidth: 1,
+    backgroundColor: '#fff2'
   }
 })
