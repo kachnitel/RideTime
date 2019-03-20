64db9ef75a036cb9fedff6772b29aba4b75056e7
Ondrej Vana <vana.ondrej@gmail.com>
2019-03-20 01:24:33 -0700

    refactor: User::createApiJson is now a function

--- stores/UserStore.mobx.js
+++ stores/UserStore.mobx.js
@@ -7,7 +7,7 @@ export default class UserStore {
   rootStore: RootStore
   @observable _users = []
 
-  constructor (rootStore: RootStore, userProvider: ?RidersProvider) {
+  constructor (rootStore: ?RootStore, userProvider: ?RidersProvider) {
     this.rootStore = rootStore
     this.provider = userProvider
   }
@@ -38,23 +38,46 @@ export default class UserStore {
 
 export class User {
   userStore: UserStore
+
+  /**
+   * API object parameter mapping
+   * [{
+   *  apiParam: 'key',
+   *  getter: getter method in this class
+   * }]
+   *
+   * @static
+   * @memberof User
+   */
+  API_PARAMS = [
+    { apiParam: 'id', getter: 'id' },
+    { apiParam: 'name', getter: 'name' },
+    { apiParam: 'email', getter: 'email' },
+    { apiParam: 'hometown', getter: 'hometown' },
+    { apiParam: 'picture', getter: 'picture' },
+    { apiParam: 'favTerrain', getter: 'bike' },
+    { apiParam: 'level', getter: 'level' },
+    { apiParam: 'locations', getter: 'locations' }
+  ]
+
   // TODO: Friends, Home Locations, ...if needed
   // Likely separate store for at least friends
+
   // ID can change when creating new user
-  @observable _id = false
-  @observable _name = ''
-  @observable _picture = ''
-  @observable _email = ''
-  @observable _hometown = ''
-  @observable _level = null
-  @observable _bike = ''
-  @observable _locations = []
+  @observable _id: Number = false
+  @observable _name: String = ''
+  @observable _picture: String = '' // Web URL
+  @observable _email: String = ''
+  @observable _hometown: String = ''
+  @observable _level: Number = null
+  @observable _bike: String = ''
+  @observable _locations: Array = []
   // TODO: phone, ...
 
   // Picture that hasn't been uploaded yet
   @observable _tempPicture = null
 
-  constructor (userStore: UserStore) {
+  constructor (userStore: ?UserStore) {
     this.userStore = userStore
   }
 
@@ -93,6 +116,7 @@ export class User {
   }
 
   @action async save () {
+    // FIXME: api JSON
     let userResponse = await this.userStore.provider.signUp({
       name: this.name,
       hometown: this.hometown,
@@ -122,7 +146,7 @@ export class User {
     this.updateTempPicture(user.tempPicture)
     this.updateLocations(user.locations)
 
-    return this.userStore.provider.updateUser(this.id, this.createApiJson)
+    return this.userStore.provider.updateUser(this.id, this.createApiJson())
   }
 
   @action populateFromApiResponse (userResponse: Object) {
@@ -136,16 +160,18 @@ export class User {
     this.updateEmail(userResponse.email)
   }
 
-  @computed get createApiJson () {
-    return {
-      id: this.id,
-      name: this.name,
-      email: this.email,
-      hometown: this.hometown,
-      picture: this.picture,
-      favTerrain: this.bike,
-      level: this.level,
-      locations: this.locations
-    }
+  /**
+   * @param {?Array} exclude Array of API params to exclude
+   * @returns Object
+   * @memberof User
+   */
+  createApiJson (exclude: ?Array) {
+    return this.API_PARAMS.reduce((result, mapping) => {
+      if (!(exclude && exclude.includes(mapping.apiParam))) {
+        result[mapping.apiParam] = this[mapping.getter]
+      }
+
+      return result
+    }, {})
   }
 }
--- stores/__tests__/UserStore.mobx-test.js
+++ stores/__tests__/UserStore.mobx-test.js
@@ -12,3 +12,24 @@ test('should add user to store', async () => {
 
   expect(await store.get(1)).toBe(user)
 })
+
+test('should return correct API object', () => {
+  let user = new User()
+
+  user.updateId(2)
+  user.updateName('Joe')
+  user.updateBike('trail')
+  user.updatePicture('http://pic.tu/re')
+
+  expect(user.createApiJson()).toMatchObject({
+    id: 2,
+    name: 'Joe',
+    favTerrain: 'trail',
+    picture: 'http://pic.tu/re'
+  })
+
+  expect(user.createApiJson(['picture', 'favTerrain'])).toMatchObject({
+    id: 2,
+    name: 'Joe'
+  })
+})
