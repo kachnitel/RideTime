9a4cff499585faf8d33f7de409bb981638c03803
Ondrej Vana <vana.ondrej@gmail.com>
2019-03-24 01:20:03 -0700

    feat: Remove RideItem from Profile (may return in the future)

--- components/profile/Profile.js
+++ components/profile/Profile.js
@@ -1,9 +1,7 @@
 import React from 'react'
-import { ScrollView, StyleSheet, View } from 'react-native'
+import { ScrollView, StyleSheet } from 'react-native'
 import Colors from '../../constants/Colors'
-import RidesProvider from '../../providers/RidesProvider'
 import CountHeader from '../CountHeader'
-import RideItem from '../list_items/RideItem'
 import { FriendList } from './FriendList'
 import ProfileHeader from './ProfileHeader'
 import { Favourites } from './Favourites'
@@ -14,37 +12,12 @@ export default
 @inject('User')
 @observer
 class Profile extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      upcomingRide: null,
-      loadingRide: true
-    }
-    this.ridesProvider = new RidesProvider()
-  }
-
-  // TODO: EventStore,get
-  async componentDidMount () {
-    if (this.props.User.events.length > 0) {
-      let upcomingRide = await this.ridesProvider.get(this.props.User.events[0].id)
-      this.setState({
-        upcomingRide: upcomingRide,
-        loadingRide: false
-      })
-    }
-  }
-
   render () {
     return (
       <ScrollView>
         <ProfileHeader user={this.props.User} />
         <FriendList friends={this.props.User.friends} style={styles.friendList} />
         <CountHeader number={this.props.User.events.length} style={styles.title}>Upcoming Rides</CountHeader>
-        { !this.state.loadingRide &&
-        <View style={styles.rideItemContainer}>
-          <RideItem ride={this.state.upcomingRide} style={styles.rideItem} />
-        </View>
-        }
         <Favourites text={this.props.User.favourites} style={styles.title} />
       </ScrollView>
     )
