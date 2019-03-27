25e711c59518511fd058d5366239dcc869bacc99
Ondrej Vana <vana.ondrej@gmail.com>
2019-03-27 00:59:50 -0700

    chore: Remove babel decorators plugin (seems to have no effect)

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,12 @@
 module.exports = function (api) {
   api.cache(true)
   return {
-    presets: ['babel-preset-expo'],
+    presets: [
+      'babel-preset-expo'
+    ],
     'sourceMaps': 'inline',
     'plugins': [
-      ['@babel/plugin-proposal-decorators', { 'legacy': true }]
+      // ['@babel/plugin-proposal-decorators', { 'decoratorsBeforeExport': true }]
     ]
   }
 }
