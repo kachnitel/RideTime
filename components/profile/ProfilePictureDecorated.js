9671719d096d09173203224c5fb400ec00e399b4
Ondrej Vana <vana.ondrej@gmail.com>
2019-03-28 02:43:09 -0700

    fix: ProfilePictureDecorated error with null level

--- components/profile/ProfilePictureDecorated.js
+++ components/profile/ProfilePictureDecorated.js
@@ -7,14 +7,21 @@ import Layout from '../../constants/Layout'
 import PropTypes from 'prop-types'
 
 export default class ProfilePictureDecorated extends Component {
-  render () {
-    return <View {...this.props}>
-      <ProfilePicture picture={this.props.user.picture} size={Layout.window.hp(7)} />
-      <View style={styles.diffIcon}>
+  renderDiffIcon = () => {
+    return this.props.user.level != null
+      ? <View style={styles.diffIcon}>
         <OutlineIcon thickness={1.075} outlineStyle={styles.outlineIcon}>
           <DifficultyIcon d={this.props.user.level} size={Layout.window.hp(3.5)} />
         </OutlineIcon>
       </View>
+      : <></>
+  }
+
+  render () {
+    return <View {...this.props}>
+      <ProfilePicture picture={this.props.user.picture} size={Layout.window.hp(7)} />
+      {/* {this.props.user.level && this.renderDiffIcon()} */}
+      {this.renderDiffIcon()}
     </View>
   }
 }
