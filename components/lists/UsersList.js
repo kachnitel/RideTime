7d94a410340a068585e9d6dc9a18ac5f015c991c
Ondrej Vana <vana.ondrej@gmail.com>
2019-04-13 00:23:28 -0700

    fix: Remove unused inject from UsersList

--- components/lists/UsersList.js
+++ components/lists/UsersList.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import { StyleSheet, View, Text } from 'react-native'
 import AlternatingStyleList from './AlternatingStyleList'
 import UserItem from '../list_items/UserItem'
-import { inject, observer } from 'mobx-react/native'
 
 /**
  * fixme Pretty well duplicated from RidersList
@@ -11,10 +10,7 @@ import { inject, observer } from 'mobx-react/native'
  * @class UsersList
  * @extends {Component}
  */
-export default
-@inject('ApplicationStore')
-@observer
-class UsersList extends Component {
+export default class UsersList extends Component {
   itemComponent = (id, style) => {
     return <UserItem id={id} style={style} actions={this.props.actions} />
   }
