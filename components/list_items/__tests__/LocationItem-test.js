5dc5216414e33b6dd7513367a915069c0420979f
Ondrej Vana <vana.ondrej@gmail.com>
2019-05-03 01:07:19 -0700

    test: LocationItem test

--- components/list_items/__tests__/LocationItem-test.js
+++ components/list_items/__tests__/LocationItem-test.js
@@ -1,8 +1,9 @@
 /* eslint-env jest */
 import React from 'react'
-import 'react-native'
 import renderer from 'react-test-renderer'
 import LocationItem from '../LocationItem'
+import locationStore from '../../../stores/LocationStore.singleton'
+import { Location } from '../../../stores/LocationStore.mobx'
 
 jest.mock('../../location/LocationDifficulties', () => 'LocationDifficulties')
 jest.mock('../../RiderCount', () => 'RiderCount')
@@ -11,13 +12,26 @@ const mockMath = Object.create(global.Math)
 mockMath.random = () => 0.5
 global.Math = mockMath
 
-it('renders correctly', () => {
+it('renders correctly', async () => {
   let testLocation = {
+    id: 1,
     name: 'Place',
     difficulties: [1, 2]
   }
 
-  const tree = renderer.create(<LocationItem location={testLocation} />).toJSON()
+  let location = new Location(locationStore)
+  location.populateFromApiResponse(testLocation)
+  locationStore.add(location)
 
-  expect(tree).toMatchSnapshot()
+  const component = renderer.create(
+    <LocationItem.wrappedComponent
+      locationId={location.id}
+      LocationStore={locationStore}
+    />
+  )
+  expect(component.toJSON()).toMatchSnapshot()
+  let instance = component.getInstance()
+  await new Promise((resolve) => setTimeout(resolve, 10))
+  expect(await instance.location).toBe(location)
+  expect(component.toJSON()).toMatchSnapshot()
 })
