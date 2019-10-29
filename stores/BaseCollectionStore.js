6a2d2cd2d08febcb7f3909693a9ab78584c99791
Ondrej Vana <vana.ondrej@gmail.com>
2019-10-29 13:38:34 -0700

    fix: turn BaseCollectionStore.upsert to arrow function
    prevents _findInCollection undefined error

--- stores/BaseCollectionStore.js
+++ stores/BaseCollectionStore.js
@@ -80,7 +80,7 @@ export class BaseCollectionStore {
    * @param {*} object API Response formatted object
    * @memberof EventStore
    */
-  upsert (object) {
+  upsert = (object) => {
     let entity = this._findInCollection(object.id) || new this.EntityClass(this)
 
     entity.populateFromApiResponse(object, true)
