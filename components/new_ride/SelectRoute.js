72784c522447fb911c4b913c56d80c416dd34f6b
Ondrej Vana <vana.ondrej@gmail.com>
2019-12-01 21:29:54 -0800

    feat: TrailFilter in SelectRoute

--- components/new_ride/SelectRoute.js
+++ components/new_ride/SelectRoute.js
@@ -3,12 +3,39 @@ import { View, StyleSheet } from 'react-native'
 import AlternatingStyleList from '../lists/AlternatingStyleList'
 import { observer } from 'mobx-react/native'
 import RouteItem from '../list_items/RouteItem'
+import TrailFilter from './TrailFilter'
+import Layout from '../../constants/Layout'
 
 export default
 @observer
 class SelectRoute extends Component {
+  state = {
+    routes: []
+  }
+
+  componentDidMount = () => {
+    this.setState({
+      routes: this.props.location.routes
+    })
+  }
+
+  handleFilterUpdate = (filter) => {
+    let routes = this.props.location.routes
+    if (filter.difficulties.length > 0) {
+      routes = routes.filter((trail) =>
+        filter.difficulties.includes(trail.difficulty))
+    }
+    if (filter.search.length > 0) {
+      routes = routes.filter((trail) =>
+        trail.title.includes(filter.search.trim()))
+    }
+    this.setState({
+      routes: routes
+    })
+  }
+
   routesList = () => <AlternatingStyleList
-    items={this.props.location.routes}
+    items={this.state.routes}
     onItemPress={(route) => this.props.onSubmit(route)}
     itemComponent={(item, style) => <RouteItem route={item} style={style} />}
     windowSize={3}
@@ -18,7 +45,10 @@ class SelectRoute extends Component {
   render () {
     return (
       <View style={styles.container}>
-        {/* <Text>TODO: Filter</Text> */}
+        <TrailFilter
+          style={styles.filter}
+          onFilterUpdate={this.handleFilterUpdate}
+        />
         {this.routesList()}
       </View>
     )
@@ -28,5 +58,9 @@ class SelectRoute extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1
+  },
+  filter: {
+    width: '100%',
+    padding: Layout.window.wp(5)
   }
 })
