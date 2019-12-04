b2b75055b5751f8d9967c9715c66655467b81d9f
Ondrej Vana <vana.ondrej@gmail.com>
2019-12-04 04:20:11 -0800

    feat: Show badge count in Drawer & DrawerButton

--- components/DrawerButton.js
+++ components/DrawerButton.js
@@ -2,28 +2,38 @@ import React from 'react'
 import { TouchableOpacity, View, StyleSheet } from 'react-native'
 import Icon from 'react-native-vector-icons/MaterialIcons'
 import Colors from '../constants/Colors'
+import CountBadge from './CountBadge'
+import stores from '../stores/CollectionStores.singleton'
 
 export default class DrawerButton extends React.Component {
   render () {
+    let badgeCount = stores.user.friendRequests.length + stores.event.invites.length
     return (
       <TouchableOpacity onPress={() => this.props.navigation.toggleDrawer()}>
         <View style={styles.headerMenuIconContainer}>
           <Icon style={styles.headerMenuIcon} name='menu' />
+          { badgeCount > 0 && <CountBadge
+            count={badgeCount}
+            style={styles.badge}
+          /> }
         </View>
       </TouchableOpacity>
     )
   };
 }
 
-const styles = StyleSheet.create(
-  {
-    headerMenuIconContainer: {
-      justifyContent: 'center'
-    },
-    headerMenuIcon: {
-      fontSize: 32,
-      padding: 15,
-      color: Colors.tintColor
-    }
+const styles = StyleSheet.create({
+  headerMenuIconContainer: {
+    justifyContent: 'center'
+  },
+  headerMenuIcon: {
+    fontSize: 32,
+    padding: 15,
+    color: Colors.tintColor
+  },
+  badge: {
+    position: 'absolute',
+    top: 10,
+    right: 10
   }
-)
+})
--- navigation/DrawerNavigator.js
+++ navigation/DrawerNavigator.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { View, Text } from 'react-native'
 import { createDrawerNavigator } from 'react-navigation'
 import HomeStack from './HomeNavigator'
 import Colors from '../constants/Colors'
@@ -6,6 +7,8 @@ import FriendStack from './FriendNavigator'
 import DrawerContent from './DrawerContent'
 import Icon from 'react-native-vector-icons/MaterialIcons'
 import Layout from '../constants/Layout'
+import CountBadge from '../components/CountBadge'
+import stores from '../stores/CollectionStores.singleton'
 
 let getIcon = (name: String, color) => <Icon
   name={name}
@@ -13,18 +16,31 @@ let getIcon = (name: String, color) => <Icon
   size={Layout.window.hp(3.5)}
 />
 
+let getLabel = (title: String, color: String, badgeCount: ?Number) =>
+  <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+    <Text style={{
+      marginVertical: Layout.window.hp(2),
+      fontWeight: 'bold',
+      color: color
+    }}>{title}</Text>
+    {!!badgeCount && <CountBadge count={badgeCount} style={{
+      marginRight: Layout.window.wp(5)
+    }} />}
+  </View>
+
 const DrawerStack = createDrawerNavigator(
   {
     Home: {
       screen: HomeStack,
       navigationOptions: {
-        drawerLabel: 'Rides',
+        drawerLabel: ({ tintColor }) => getLabel('Rides', tintColor, stores.event.invites.length),
         drawerIcon: ({ tintColor }) => getIcon('view-list', tintColor)
       }
     },
     Friends: {
       screen: FriendStack,
       navigationOptions: {
+        drawerLabel: ({ tintColor }) => getLabel('Friends', tintColor, stores.user.friendRequests.length),
         drawerIcon: ({ tintColor }) => getIcon('people-outline', tintColor)
       }
     }
