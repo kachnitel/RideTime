24c95b2d546642efbf43d0895645e17b350c16a6
Ondrej Vana <vana.ondrej@gmail.com>
2019-12-25 20:54:34 -0800

    feat: `icon` & `description` prop support in MenuModalOption

--- src/components/modal/MenuModalOption.js
+++ src/components/modal/MenuModalOption.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types'
 import React, { Component } from 'react'
 import { View, Text, StyleSheet, TouchableNativeFeedback } from 'react-native'
+import Icon from 'react-native-vector-icons/MaterialIcons'
 import Colors from '../../../constants/Colors'
 import Layout from '../../../constants/Layout'
 
@@ -10,8 +11,16 @@ export default class MenuModalOption extends Component {
       <TouchableNativeFeedback
         onPress={this.props.onPress}
       >
-        <View style={{ ...styles.optionContainer, ...this.props.style }}>
-          <Text>{this.props.label}</Text>
+        <View style={{ ...styles.container, ...this.props.style }}>
+          {this.props.icon && <Icon
+            name={this.props.icon}
+            size={Layout.window.hp(4)}
+            style={styles.icon}
+          />}
+          <View style={styles.textContainer}>
+            <Text style={styles.label}>{this.props.label}</Text>
+            {this.props.description && <Text style={styles.description} numberOfLines={1}>{this.props.description}</Text>}
+          </View>
         </View>
       </TouchableNativeFeedback>
     )
@@ -19,17 +28,35 @@ export default class MenuModalOption extends Component {
 }
 
 MenuModalOption.propTypes = {
+  description: PropTypes.string,
+  icon: PropTypes.string,
   label: PropTypes.string,
   onPress: PropTypes.func,
   style: PropTypes.any
 }
 
 const styles = StyleSheet.create({
-  optionContainer: {
-    borderBottomColor: Colors.darkBackground,
-    borderBottomWidth: 1,
-    paddingVertical: Layout.window.hp(3),
+  container: {
+    padding: Layout.window.hp(3),
     width: '100%',
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  icon: {
+    opacity: 0.5,
+    color: Colors.tintColor
+  },
+  label: {
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+  description: {
+    opacity: 0.6,
+    textAlign: 'center'
+  },
+  textContainer: {
+    flex: 1,
     alignItems: 'center'
   }
 })
