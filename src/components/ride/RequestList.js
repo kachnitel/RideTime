89be0a7513f401349726a91ad003fa61cd0585d4
Ondrej Vana <vana.ondrej@gmail.com>
2019-12-28 02:05:40 -0800

    feat: Tap user in RequestList to show his detail

--- src/components/ride/RequestList.js
+++ src/components/ride/RequestList.js
@@ -1,11 +1,17 @@
 import PropTypes from 'prop-types'
 import React, { Component } from 'react'
-import { StyleSheet, Text, FlatList, View } from 'react-native'
+import { StyleSheet, Text, FlatList, View, TouchableOpacity } from 'react-native'
 import UserItem from '../user/UserItem'
 import InviteChoices from './InviteChoices'
 import { Event } from '../../stores/EventStore.mobx'
+import ProfileHeader from '../profile/ProfileHeader'
+import { inject, observer } from 'mobx-react/native'
+import Layout from '../../../constants/Layout'
 
-export default class RequestList extends Component {
+export default
+@inject('UserStore')
+@observer
+class RequestList extends Component {
   state = {
     detail: null
   }
@@ -18,24 +24,39 @@ export default class RequestList extends Component {
     this.props.event.loadRequested()
   }
 
-  itemComponent = ({ item }) => <View>
-    <UserItem id={item} />
-    {/* TODO: Message option */}
-    <InviteChoices
-      options={[
-        {
-          icon: 'person-add',
-          label: 'Accept',
-          action: () => this.props.event.acceptRequest(item)
-        },
-        {
-          icon: 'clear',
-          label: 'Dismiss',
-          action: () => this.props.event.declineRequest(item),
-          fade: true
-        }
-      ]}
+  /* TODO: Message option */
+  inviteChoices = (id) => {
+    let options = [
+      {
+        icon: 'person-add',
+        label: 'Accept',
+        action: () => this.props.event.acceptRequest(id)
+      },
+      {
+        icon: 'clear',
+        label: 'Dismiss',
+        action: () => this.props.event.declineRequest(id),
+        fade: true
+      }
+    ]
+
+    if (this.state.detail) {
+      options.push({
+        icon: 'arrow-back',
+        label: 'Back',
+        action: () => this.setState({ detail: null })
+      })
+    }
+    return <InviteChoices
+      options={options}
     />
+  }
+
+  itemComponent = ({ item }) => <View>
+    <TouchableOpacity onPress={() => this.setState({ detail: item })}>
+      <UserItem id={item} />
+    </TouchableOpacity>
+    {this.inviteChoices(item)}
   </View>
 
   listComponent = () => <FlatList
@@ -48,10 +69,22 @@ export default class RequestList extends Component {
     extraData={this.props.event.requested.length}
   />
 
+  detailComponent = () => <View>
+    <View style={styles.detailComponentProfile}>
+
+      <ProfileHeader
+        user={this.props.UserStore.getSync(this.state.detail)}
+      />
+    </View>
+    {this.inviteChoices(this.state.detail)}
+  </View>
+
   render () {
     return <View style={styles.container}>
-      {/* {this.state.detail ? : } */}
-      {this.listComponent()}
+      {this.state.detail
+        ? this.detailComponent()
+        : this.listComponent()
+      }
     </View>
   }
 }
@@ -63,5 +96,10 @@ RequestList.propTypes = {
 const styles = StyleSheet.create({
   container: {
     width: '100%'
+  },
+  detailComponentProfile: {
+    paddingHorizontal: Layout.window.hp(1),
+    paddingBottom: Layout.window.hp(1),
+    borderRadius: Layout.window.hp(5)
   }
 })
