6f45768972a63e772e647b6dcf3808b92989c24d
Ondrej Vana <vana.ondrej@gmail.com>
2020-01-14 18:01:44 -0800

    fix: Show empty view if location has no cover picture

--- src/components/location/CoverPicture.js
+++ src/components/location/CoverPicture.js
@@ -1,12 +1,14 @@
 import PropTypes from 'prop-types'
 import React from 'react'
-import { Image } from 'react-native'
+import { Image, View } from 'react-native'
 
 const CoverPicture = (props) => (
-  <Image // TODO: Better link! pNpb where N seems to stand for size
-    source={{ uri: `https://ep1.pinkbike.org/p2pb${props.id}/p2pb${props.id}.jpg` }}
-    {...props}
-  />
+  props.id
+    ? <Image // TODO: Better link! pNpb where N seems to stand for size
+      source={{ uri: `https://ep1.pinkbike.org/p2pb${props.id}/p2pb${props.id}.jpg` }}
+      {...props}
+    />
+    : <View {...props} />
 )
 
 CoverPicture.propTypes = {
