0279bbacc3b5297a2926023d6a612d4565dc92d9
Ondrej Vana <vana.ondrej@gmail.com>
2020-02-08 22:14:54 -0800

    chore: Update events list API call to API 0.9.5
    fixes #164

--- src/providers/RidesProvider.js
+++ src/providers/RidesProvider.js
@@ -2,7 +2,7 @@ import ApiConnection from '../ApiConnection'
 
 export default class RidesProvider {
   addRide = (data) => ApiConnection.post('api/events', data)
-  list = (ids: Number[]) => ApiConnection.get('api/events', { ids })
+  list = (ids: Number[]) => ApiConnection.get('api/events/filter', { id: ids })
   filter = (filters: Object) => ApiConnection.get('api/events/filter', filters)
   get = (id) => ApiConnection.get('api/events/' + id)
 
