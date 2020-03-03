f3e38b96985b2e754642a9cc09ef601a9b981271
Ondrej Vana <vana.ondrej@gmail.com>
2020-03-02 19:34:35 -0800

    feat: Simpler UserMarker in tracking map

--- src/components/tracking/UserMarker.js
+++ src/components/tracking/UserMarker.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import { View, StyleSheet } from 'react-native'
 import { Marker } from 'react-native-maps'
 import Layout from '../../../constants/Layout'
 import Colors from '../../../constants/Colors'
@@ -20,18 +20,13 @@ const UserMarker = ({ coords, user, color, onCalloutPress }) => {
   >
     <View style={{
       ...styles.container,
-      backgroundColor: color ?? Colors.darkBackgroundTransparent
+      backgroundColor: color ?? Colors.darkBackgroundTransparent,
+      borderColor: color ?? Colors.darkBackgroundTransparent
     }}>
       <ProfilePicture
         style={styles.picture}
         picture={user.picture}
       />
-      <Text
-        style={styles.name}
-        numberOfLines={1}
-      >
-        {user.name.charAt(0)}
-      </Text>
     </View>
   </Marker>
 }
@@ -41,20 +36,15 @@ export default UserMarker
 const styles = StyleSheet.create({
   container: {
     alignItems: 'center',
-    borderRadius: Layout.window.hp(2),
+    borderRadius: Layout.window.hp(2.5),
     width: Layout.window.hp(5),
+    borderWidth: Layout.window.hp(0.25),
+    aspectRatio: 1,
     overflow: 'hidden',
     flex: 1
   },
   picture: {
     width: '100%',
-    aspectRatio: 1.2,
     resizeMode: 'cover'
-  },
-  name: {
-    fontSize: Layout.window.hp(1.5),
-    color: Colors.secondaryText,
-    paddingHorizontal: Layout.window.wp(1),
-    height: Layout.window.hp(3)
   }
 })
